4701a5a719e9ca91fc7a381651563438057eea79
mldamico <matias.lioneldamico@gmail.com>
2022-11-30 15:43:43 -0300

    Block endpoints for admin only

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -4,23 +4,37 @@ import { User } from './entities/user.entity';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import { ValidRolesArgs } from './dto/args/roles.arg';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuards } from '../auth/guards/jwt-auth.guard';
+import { currentUser } from '../auth/decorators/current-user.decorator';
+import { ValidRoles } from '../auth/enums/valid-roles.enum';
 
 @Resolver(() => User)
+@UseGuards(JwtAuthGuards)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Query(() => [User], { name: 'users' })
-  findAll(@Args() validRoles: ValidRolesArgs): Promise<User[]> {
+  findAll(
+    @Args() validRoles: ValidRolesArgs,
+    @currentUser([ValidRoles.ADMIN]) user: User,
+  ): Promise<User[]> {
     return this.usersService.findAll(validRoles.roles);
   }
 
   @Query(() => User, { name: 'user' })
-  findOne(@Args('id', { type: () => String }) email: string): Promise<User> {
+  findOne(
+    @Args('id', { type: () => String }) email: string,
+    @currentUser([ValidRoles.ADMIN]) user: User,
+  ): Promise<User> {
     return this.usersService.findOneByEmail(email);
   }
 
   @Mutation(() => User)
-  blockUser(@Args('id', { type: () => ID }) id: string): Promise<User> {
+  blockUser(
+    @Args('id', { type: () => ID }) id: string,
+    @currentUser([ValidRoles.ADMIN]) user: User,
+  ): Promise<User> {
     return this.usersService.block(id);
   }
 }
