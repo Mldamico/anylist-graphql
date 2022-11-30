6e364e13784131aa10e5fee1633a11e3574e67d9
mldamico <matias.lioneldamico@gmail.com>
2022-11-30 15:27:05 -0300

    Add enums in graphql schema

--- src/auth/enums/valid-roles.enum.ts
+++ src/auth/enums/valid-roles.enum.ts
@@ -1,5 +1,9 @@
+import { registerEnumType } from '@nestjs/graphql';
+
 export enum ValidRoles {
   ADMIN = 'ADMIN',
   USER = 'USER',
   SUPERUSER = 'SUPERUSER',
 }
+
+registerEnumType(ValidRoles, { name: 'ValidRoles' });
