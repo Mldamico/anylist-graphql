04d713d1d2bd84f6c5a66dc52b896e9558ff5938
mldamico <matias.lioneldamico@gmail.com>
2022-12-01 16:47:12 -0300

    Seed folders and protection

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class SeedService {
+  private isProd: boolean;
+  constructor(private readonly configService: ConfigService) {
+    this.isProd = configService.get('STATE') === 'prod';
+  }
+
+  async executeSeed() {
+    if (this.isProd) throw new UnauthorizedException('Cannot run seed on prod');
+    return true;
+  }
+}
