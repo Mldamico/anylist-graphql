288fead49b9d6c429b3bea94ec45c3c51825344f
mldamico <matias.lioneldamico@gmail.com>
2022-12-02 18:40:56 -0300

    Filters items users

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -17,6 +17,9 @@ import { JwtAuthGuards } from '../auth/guards/jwt-auth.guard';
 import { currentUser } from '../auth/decorators/current-user.decorator';
 import { ValidRoles } from '../auth/enums/valid-roles.enum';
 import { ItemsService } from '../items/items.service';
+import { Item } from '../items/entities/item.entity';
+import { PaginationArgs } from '../common/dto/args/pagination.args';
+import { SearchArgs } from '../common/dto/args/search.args';
 
 @Resolver(() => User)
 @UseGuards(JwtAuthGuards)
@@ -64,5 +67,15 @@ export class UsersResolver {
     @currentUser([ValidRoles.ADMIN]) adminUser: User,
   ): Promise<number> {
     return this.itemsService.itemCountByUser(user);
+  }
+
+  @ResolveField(() => Item, { name: 'items' })
+  async items(
+    @Parent() user: User,
+    @Args() paginationArgs: PaginationArgs,
+    @Args() searchArgs: SearchArgs,
+    @currentUser([ValidRoles.ADMIN]) adminUser: User,
+  ): Promise<Item[]> {
+    return this.itemsService.findAll(user, paginationArgs, searchArgs);
   }
 }
